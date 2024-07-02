09fef57e88c6a5a7c3cb611e2a3c6e583d12ff2a
VictorienMarette <victorienmarette@gmail.com>
2024-07-02 15:02:09 +0200

    finition de la visu

--- visualisations/visualisationRPS_2_A.py
+++ visualisations/visualisationRPS_2_A.py
@@ -5,45 +5,22 @@ from matplotlib.patches import Patch
 
 # Create a figure and a 3D axis
 limite = 6
+ylimite = 1
+zlimite = 3
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
 def plotwithtrans(x,y,z, color):
-    # point et droite
+    # point 
     ax.scatter([3+x,-3+x], [-3+y,0+y], [0+z,3+z], color=color)
-    ax.plot(np.array([3+x,-3+x]), np.array([-3+y,0+y]), np.array([0+z,3+z]), alpha=0.5, color=color)
-
-    #plan 1
-    points = np.array([
-        [3+x, -3+y, 0+z],  # Point A
-        [-3+x, 0+y, 3+z], # Point B
-        [-3+x, 0+y, 1.5*limite],# Point C
-        [3+x, -3+y, 1.5*limite]  # Point D
-    ])
-    x1, y1, z1 = points[:, 0], points[:, 1], points[:, 2]
-    vertices = [list(zip(x1, y1, z1))]
-    poly = Poly3DCollection(vertices, alpha=0.5, facecolor=color, edgecolor=color)
-    ax.add_collection3d(poly)
-
-    #plan 2
-    points = np.array([
-        [3+x, -3+y, 0+z],  # Point A
-        [-3+x, 0+y, 3+z], # Point B
-        [-3+x, 1.5*limite, 3+z],# Point C
-        [3+x, 1.5*limite, 0+z]  # Point D
-    ])
-    x1, y1, z1 = points[:, 0], points[:, 1], points[:, 2]
-    vertices = [list(zip(x1, y1, z1))]
-    poly = Poly3DCollection(vertices, alpha=0.5, facecolor=color, edgecolor=color)
-    ax.add_collection3d(poly)
 
     #plan 3
     points = np.array([
         [3+x, -3+y, 0+z],  # Point A
         [1.5*limite, -3+y, 0+z], # Point B
-        [1.5*limite, 1.5*limite, 0+z],# Point C
-        [3+x, 1.5*limite, 0+z]  # Point D
+        [1.5*limite, 1.5*ylimite, 0+z],# Point C
+        [3+x, 1.5*ylimite, 0+z]  # Point D
     ])
     x1, y1, z1 = points[:, 0], points[:, 1], points[:, 2]
     vertices = [list(zip(x1, y1, z1))]
@@ -54,8 +31,8 @@ def plotwithtrans(x,y,z, color):
     points = np.array([
         [3+x, -3+y, 0+z],  # Point A
         [1.5*limite, -3+y, 0+z], # Point B
-        [1.5*limite, -3+y, 1.5*limite],# Point C
-        [3+x, -3+y, 1.5*limite]  # Point D
+        [1.5*limite, -3+y, 1.5*zlimite],# Point C
+        [3+x, -3+y, 1.5*zlimite]  # Point D
     ])
     x1, y1, z1 = points[:, 0], points[:, 1], points[:, 2]
     vertices = [list(zip(x1, y1, z1))]
@@ -65,9 +42,9 @@ def plotwithtrans(x,y,z, color):
     #plan 5
     points = np.array([
         [-3+x, 0+y, 3+z],  # Point A
-        [-3+x, 0+y, 1.5*limite], # Point B
-        [-3+x, 1.5*limite, 1.5*limite],# Point C
-        [-3+x, 1.5*limite, 3+z]  # Point D
+        [-3+x, 0+y, 1.5*zlimite], # Point B
+        [-3+x, 1.5*ylimite, 1.5*zlimite],# Point C
+        [-3+x, 1.5*ylimite, 3+z]  # Point D
     ])
     x1, y1, z1 = points[:, 0], points[:, 1], points[:, 2]
     vertices = [list(zip(x1, y1, z1))]
@@ -80,10 +57,84 @@ plotwithtrans(0,-1,-2, "b")
 #E pour le signal Paper s translation de (-2,0,-1)
 plotwithtrans(-2,0,-1, "r")
 
-origin = [0, 0, 0]
-ax.quiver(*origin, 2, 0, 0, color='red', arrow_length_ratio=0.1, label='X axis')
-ax.quiver(*origin, 0, 2, 0, color='green', arrow_length_ratio=0.1, label='Y axis')
-ax.quiver(*origin, 0, 0, 2, color='blue', arrow_length_ratio=0.1, label='Z axis')
+
+#plot droites
+ax.plot(np.array([-3, -5]), np.array([-1, 0]), np.array([1,2]), linewidth=3,alpha=0.5, color="r")
+ax.plot(np.array([3, 1]), np.array([-4, -3]), np.array([-2,-1]),linewidth=3, alpha=0.5, color="b")
+ax.plot(np.array([-3, 1]), np.array([-1, -3]), np.array([1,-1]),linewidth=3, alpha=0.5, color="purple")
+
+#plan 1
+
+points = np.array([
+        [-3, -1, 1],  # Point A
+        [-5, 0, 2], # Point B
+        [-5, 0, 1.5*zlimite],# Point C
+        [-3, -1, 1.5*zlimite]  # Point D
+    ])
+x1, y1, z1 = points[:, 0], points[:, 1], points[:, 2]
+vertices = [list(zip(x1, y1, z1))]
+poly = Poly3DCollection(vertices, alpha=0.5, facecolor="r", edgecolor="r")
+ax.add_collection3d(poly)
+
+points = np.array([
+        [3, -4, -2],  # Point A
+        [1, -3, -1], # Point B
+        [1, -3, 1.5*zlimite],# Point C
+        [3, -4, 1.5*zlimite]  # Point D
+    ])
+x1, y1, z1 = points[:, 0], points[:, 1], points[:, 2]
+vertices = [list(zip(x1, y1, z1))]
+poly = Poly3DCollection(vertices, alpha=0.5, facecolor="b", edgecolor="b")
+ax.add_collection3d(poly)
+
+points = np.array([
+        [-3, -1, 1],  # Point A
+        [1, -3, -1], # Point B
+        [1, -3, 1.5*zlimite],# Point C
+        [-3, -1, 1.5*zlimite]  # Point D
+    ])
+x1, y1, z1 = points[:, 0], points[:, 1], points[:, 2]
+vertices = [list(zip(x1, y1, z1))]
+poly = Poly3DCollection(vertices, alpha=0.5, facecolor="purple", edgecolor="purple")
+ax.add_collection3d(poly)
+
+
+#plan 2
+points = np.array([
+    [-3, -1, 1],  # Point A
+    [-5, 0, 2], # Point B
+    [-5, 1.5*ylimite, 2],# Point C
+    [-3, 1.5*ylimite, 1]  # Point D
+])
+x1, y1, z1 = points[:, 0], points[:, 1], points[:, 2]
+vertices = [list(zip(x1, y1, z1))]
+poly = Poly3DCollection(vertices, alpha=0.5, facecolor="r", edgecolor="r")
+ax.add_collection3d(poly)
+
+
+points = np.array([
+    [3, -4, -2],  # Point A
+    [1, -3, -1], # Point B
+    [1, 1.5*ylimite, -1],# Point C
+    [3, 1.5*ylimite, -2]  # Point D
+])
+x1, y1, z1 = points[:, 0], points[:, 1], points[:, 2]
+vertices = [list(zip(x1, y1, z1))]
+poly = Poly3DCollection(vertices, alpha=0.5, facecolor="b", edgecolor="b")
+ax.add_collection3d(poly)
+
+
+points = np.array([
+    [-3, -1, 1],  # Point A
+    [1, -3, -1], # Point B
+    [1, 1.5*ylimite, -1],# Point C
+    [-3, 1.5*ylimite, 1]  # Point D
+])
+x1, y1, z1 = points[:, 0], points[:, 1], points[:, 2]
+vertices = [list(zip(x1, y1, z1))]
+poly = Poly3DCollection(vertices, alpha=0.5, facecolor="purple", edgecolor="purple")
+ax.add_collection3d(poly)
+
 
 
 # Define custom legend patches
@@ -96,8 +147,8 @@ plt.legend(handles=[blue_patch, red_patch])
 
 # Set plot limits
 ax.set_xlim([-limite, limite])
-ax.set_ylim([-limite, limite])
-ax.set_zlim([-limite, limite])
+ax.set_ylim([-4, 1])
+ax.set_zlim([-3, 3])
 
 # Set labels
 ax.set_xlabel('V rock')
